625e92c3552434b6c8fae01468ee887cdfcdfda0
Nikshitha Shetty <nikshitha.s@sonata-software.com>
2023-11-15 16:18:50 +0530

    added footer

---
+++ src/root.component.tsx
@@ -0,0 +1,10 @@
+import Footer from './component/Footer.js';
+import ReactDOM from 'react-dom';
+import FooterContainer from "./component/FooterContainer.js"
+
+
+export default function Root(props) {
+  return (
+    
+  <FooterContainer/>);
+}
